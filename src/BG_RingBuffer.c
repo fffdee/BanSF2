737a3e26d3d74db7ad5ff81a9bc0982bb25edbc8
BanGO <695371519@qq.com>
2025-04-03 00:43:38 +0800

    finish ringbuffer

--- src/BG_RingBuffer.c
+++ src/BG_RingBuffer.c
@@ -1,18 +1,87 @@
 #include "BG_RingBuffer.h"
+#include <string.h>
 
 
-static uint16_t ringbuffer_get_data(struct BG_RingBuffer *buffer, uint8_t *data, uint16_t size )
+
+static uint16_t ringbuffer_get_remian(struct BG_RingBuffer *ringbuffer)
 {
+    uint16_t size = 0;
+    if (ringbuffer->Data.data_start > ringbuffer->Data.data_over)
+    {
+        size = (ringbuffer->Data.max_size - ringbuffer->Data.data_start) + ringbuffer->Data.data_over;
+    }
+    else
+    {
+        size = ringbuffer->Data.data_over - ringbuffer->Data.data_start;
+    }
+    return size;
+}
+
+
+static uint16_t ringbuffer_get_data(struct BG_RingBuffer *ringbuffer, uint8_t *data, uint16_t size)
+{
+    uint16_t count = 0;
+    uint16_t real_size = 0;
+
+    ringbuffer->Data.data_count = ringbuffer->Data.max_size - ringbuffer_get_remian(ringbuffer);
+
+    if(size > ringbuffer->Data.data_count )
+    {
+        real_size = ringbuffer->Data.data_count ;
+    }else
+    {
+        real_size = size;
+    }
+
+    for (count = 0; count < real_size ; count++)
+    {
+        uint16_t index = count + ringbuffer->Data.data_start;
+        if (index > ringbuffer->Data.max_size)
+        {
+            index = index - ringbuffer->Data.max_size;
+        }
+        data[count] = ringbuffer->Data.buffer[index];
+    }
+    ringbuffer->Data.data_start = ringbuffer->Data.data_start + real_size;
+    if(ringbuffer->Data.data_start>ringbuffer->Data.max_size){
+        ringbuffer->Data.data_start = ringbuffer->Data.data_start - ringbuffer->Data.max_size;
+    }
+    return real_size;
 
 }
 
-static void ringbuffer_set_data(struct BG_RingBuffer *buffer, uint8_t *data, uint16_t size )
+static uint16_t ringbuffer_set_data(struct BG_RingBuffer *ringbuffer, uint8_t *data, uint16_t size)
 {
+    uint8_t count = 0;
+    uint16_t real_size = 0;
 
+    if(size > ringbuffer_get_remian(ringbuffer) )
+    {
+        real_size = ringbuffer_get_remian(ringbuffer) ;
+    }else
+    {
+        real_size = size;
+    }
+    for (count = 0; count < real_size; count++)
+    {
+        uint16_t index = count + ringbuffer->Data.data_over;
+        if (index > ringbuffer->Data.max_size)
+        {
+            index = index - ringbuffer->Data.max_size;
+        }
+        ringbuffer->Data.buffer[index] = data[count];
+    }
+    return real_size;
 }
 
+static void ringbuffer_clean_buffer(struct BG_RingBuffer *ringbuffer)
+{
+    memset(ringbuffer->Data.buffer, 0, ringbuffer->Data.max_size);
+    ringbuffer->Data.data_over = 0;
+    ringbuffer->Data.data_start = 0;
+}
 
-BG_RingBuffer BG_RingBuffer_Init(uint8_t *buffer,uint16_t size)
+BG_RingBuffer BG_RingBuffer_Init(uint8_t *buffer, uint16_t size)
 {
     BG_RingBuffer BG_Buffer = {
 
@@ -20,10 +89,11 @@ BG_RingBuffer BG_RingBuffer_Init(uint8_t *buffer,uint16_t size)
         .Data.max_size = size,
         .Data.data_over = 0,
         .Data.data_start = 0,
-    
-
+        .SetData = ringbuffer_set_data,
+        .GetData = ringbuffer_get_data,
+        .GetRemian = ringbuffer_get_remian,
+        .CleanBuffer = ringbuffer_clean_buffer,
     };
-        
 
+    return BG_Buffer;
 }
-
--- src/BG_RingBuffer.h
+++ src/BG_RingBuffer.h
@@ -8,24 +8,25 @@ typedef struct {
     uint8_t *buffer;
     uint16_t data_start;
     uint16_t data_over;
-    uint16_t remain_count;
+    uint16_t data_count;
     uint16_t max_size;
 
 }BG_RingBuffer_Data;
 
-typedef struct ringbuffer{
+typedef struct BG_RingBuffer{
 
     BG_RingBuffer_Data Data;
 
-    void (*SetData)(struct ringbuffer*, uint8_t *Data, uint16_t total);
+    uint16_t (*SetData)(struct ringbuffer*, uint8_t *Data, uint16_t total);
     uint16_t (* GetData)(struct ringbuffer*,uint8_t *Data, uint16_t total);
     uint16_t (* GetRemian)(struct ringbuffer*);
+    void (* CleanBuffer)(struct ringbuffer*);
 
 
 }BG_RingBuffer;
 
 
-BG_RingBuffer BG_RingBuffer_Init(uint16_t buffer_size);
+BG_RingBuffer BG_RingBuffer_Init(uint8_t  *buffer, uint16_t buffer_size);
 
 
 #endif
